127bf8fd2af6ec42ce3b4e710a502e8fff247afb
Orkhansha <orkhansha@code.edu.az>
2022-10-03 00:34:54 +0400

    Added Asp.net tasks and practice files

---
+++ Asp.Net-Intro/Asp.Net-Intro/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.Net_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public string Index(int id, string name)
+        //{
+        //    return "Hello P130" + "-" + id + "-" + name;
+        //}
+
+        public IActionResult Index(int id, string name)
+        {
+            //return Json(new { name= "Orkhan", surname = "Akbarov"});
+            //return Content("Salamlar");
+            return View();
+        }
+
+        public int Age()
+        {
+            return 28;
+        }
+
+        public string FullData(string name, string surname, int age)
+        {
+            return $"Name: {name} Surname: {surname} Age: {age}";
+        }
+    }
+}
